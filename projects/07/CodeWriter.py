0ea867d62365d0c9e71c02345a2e54d073df4f5a
Liel Amar <liel@lielamar.com>
2022-10-18 21:27:23 +0300

    Submitted exercises 1-6

--- projects/07/CodeWriter.py
+++ projects/07/CodeWriter.py
@@ -1,142 +1,154 @@
-"""This file is part of nand2tetris, as taught in The Hebrew University,
+"""
+This file is part of nand2tetris, as taught in The Hebrew University,
 and was written by Aviv Yaish according to the specifications given in  
 https://www.nand2tetris.org (Shimon Schocken and Noam Nisan, 2017)
 and as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0 
 Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
 """
+
 import typing
 
-
 class CodeWriter:
-    """Translates VM commands into Hack assembly code."""
+  """
+  Translates VM commands into Hack assembly code.
+  """
 
-    def __init__(self, output_stream: typing.TextIO) -> None:
-        """Initializes the CodeWriter.
+  def __init__(self, output_stream: typing.TextIO) -> None:
+    """Initializes the CodeWriter.
 
-        Args:
-            output_stream (typing.TextIO): output stream.
-        """
-        # Your code goes here!
-        pass
+      Args:
+        output_stream (typing.TextIO): output stream.
+    """
+    # Your code goes here!
+    pass
 
-    def set_file_name(self, filename: str) -> None:
-        """Informs the code writer that the translation of a new VM file is 
-        started.
+  def set_file_name(self, filename: str) -> None:
+    """
+    Informs the code writer that the translation of a new VM file is started.
 
-        Args:
-            filename (str): The name of the VM file.
-        """
-        # Your code goes here!
-        # This function is useful when translating code that handles the
-        # static segment.
-        # To avoid problems with Linux/Windows/MacOS differences with regards
-        # to filenames and paths, you are advised to parse the filename in
-        # the function "translate_file" in Main.py using python's os library,
-        # For example, using code similar to:
-        # input_filename, input_extension = os.path.splitext(os.path.basename(input_file.name))
-        pass
+      Args:
+        filename (str): The name of the VM file.
+    """
+    # Your code goes here!
+    # This function is useful when translating code that handles the
+    # static segment.
+    # To avoid problems with Linux/Windows/MacOS differences with regards
+    # to filenames and paths, you are advised to parse the filename in
+    # the function "translate_file" in Main.py using python's os library,
+    # For example, using code similar to:
+    # input_filename, input_extension = os.path.splitext(os.path.basename(input_file.name))
+    pass
 
-    def write_arithmetic(self, command: str) -> None:
-        """Writes assembly code that is the translation of the given 
-        arithmetic command. For the commands eq, lt, gt, you should correctly
-        compare between all numbers our computer supports, and we define the
-        value "true" to be -1, and "false" to be 0.
+  def write_arithmetic(self, command: str) -> None:
+    """
+    Writes assembly code that is the translation of the given
+    arithmetic command. For the commands eq, lt, gt, you should correctly
+    compare between all numbers our computer supports, and we define the
+    value "true" to be -1, and "false" to be 0.
 
-        Args:
-            command (str): an arithmetic command.
-        """
-        # Your code goes here!
-        pass
+      Args:
+        command (str): an arithmetic command.
+    """
+    # Your code goes here!
+    pass
 
-    def write_push_pop(self, command: str, segment: str, index: int) -> None:
-        """Writes assembly code that is the translation of the given 
-        command, where command is either C_PUSH or C_POP.
+  def write_push_pop(self, command: str, segment: str, index: int) -> None:
+    """
+    Writes assembly code that is the translation of the given
+    command, where command is either C_PUSH or C_POP.
 
-        Args:
-            command (str): "C_PUSH" or "C_POP".
-            segment (str): the memory segment to operate on.
-            index (int): the index in the memory segment.
-        """
-        # Your code goes here!
-        # Note: each reference to static i appearing in the file Xxx.vm should
-        # be translated to the assembly symbol "Xxx.i". In the subsequent
-        # assembly process, the Hack assembler will allocate these symbolic
-        # variables to the RAM, starting at address 16.
-        pass
+      Args:
+        command (str): "C_PUSH" or "C_POP".
+        segment (str): the memory segment to operate on.
+        index (int): the index in the memory segment.
+    """
+    # Your code goes here!
+    # Note: each reference to static i appearing in the file Xxx.vm should
+    # be translated to the assembly symbol "Xxx.i". In the subsequent
+    # assembly process, the Hack assembler will allocate these symbolic
+    # variables to the RAM, starting at address 16.
+    pass
 
-    def write_label(self, label: str) -> None:
-        """Writes assembly code that affects the label command. 
-        Let "foo" be a function within the file Xxx.vm. The handling of
-        each "label bar" command within "foo" generates and injects the symbol
-        "Xxx.foo$bar" into the assembly code stream.
-        When translating "goto bar" and "if-goto bar" commands within "foo",
-        the label "Xxx.foo$bar" must be used instead of "bar".
+  def write_label(self, label: str) -> None:
+    """
+    Writes assembly code that affects the label command.
+    Let "foo" be a function within the file Xxx.vm. The handling of
+    each "label bar" command within "foo" generates and injects the symbol
+    "Xxx.foo$bar" into the assembly code stream.
+    When translating "goto bar" and "if-goto bar" commands within "foo",
+    the label "Xxx.foo$bar" must be used instead of "bar".
 
-        Args:
-            label (str): the label to write.
-        """
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        pass
-    
-    def write_goto(self, label: str) -> None:
-        """Writes assembly code that affects the goto command.
+      Args:
+        label (str): the label to write.
+    """
+    # This is irrelevant for project 7,
+    # you will implement this in project 8!
+    pass
 
-        Args:
-            label (str): the label to go to.
-        """
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        pass
-    
-    def write_if(self, label: str) -> None:
-        """Writes assembly code that affects the if-goto command. 
+  def write_goto(self, label: str) -> None:
+    """
+    Writes assembly code that affects the goto command.
 
-        Args:
-            label (str): the label to go to.
-        """
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        pass
-    
-    def write_function(self, function_name: str, n_vars: int) -> None:
-        """Writes assembly code that affects the function command. 
-        The handling of each "function foo" command within the file Xxx.vm
-        generates and injects a symbol "Xxx.foo" into the assembly code stream,
-        that labels the entry-point to the function's code.
-        In the subsequent assembly process, the assembler translates this 
-        symbol into the physical address where the function code starts.
+      Args:
+        label (str): the label to go to.
+    """
+    # This is irrelevant for project 7,
+    # you will implement this in project 8!
+    pass
 
-        Args:
-            function_name (str): the name of the function.
-            n_vars (int): the number of local variables of the function.
-        """
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        pass
-    
-    def write_call(self, function_name: str, n_args: int) -> None:
-        """Writes assembly code that affects the call command. 
-        Let "foo" be a function within the file Xxx.vm.
-        The handling of each "call" command within foo's code generates and
-        injects a symbol "Xxx.foo$ret.i" into the assembly code stream, where
-        "i" is a running integer (one such symbol is generated for each "call"
-        command within "foo").
-        This symbol is used to mark the return address within the caller's 
-        code. In the subsequent assembly process, the assembler translates this
-        symbol into the physical memory address of the command immediately
-        following the "call" command.
+  def write_if(self, label: str) -> None:
+    """
+    Writes assembly code that affects the if-goto command.
 
-        Args:
-            function_name (str): the name of the function to call.
-            n_args (int): the number of arguments of the function.
-        """
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        pass
-    
-    def write_return(self) -> None:
-        """Writes assembly code that affects the return command."""
-        # This is irrelevant for project 7,
-        # you will implement this in project 8!
-        pass
+      Args:
+        label (str): the label to go to.
+    """
+    # This is irrelevant for project 7,
+    # you will implement this in project 8!
+    pass
+
+  def write_function(self, function_name: str, n_vars: int) -> None:
+    """
+    Writes assembly code that affects the function command.
+    The handling of each "function foo" command within the file Xxx.vm
+    generates and injects a symbol "Xxx.foo" into the assembly code stream,
+    that labels the entry-point to the function's code.
+    In the subsequent assembly process, the assembler translates this
+    symbol into the physical address where the function code starts.
+
+      Args:
+        function_name (str): the name of the function.
+        n_vars (int): the number of local variables of the function.
+    """
+    # This is irrelevant for project 7,
+    # you will implement this in project 8!
+    pass
+
+  def write_call(self, function_name: str, n_args: int) -> None:
+    """
+    Writes assembly code that affects the call command.
+    Let "foo" be a function within the file Xxx.vm.
+    The handling of each "call" command within foo's code generates and
+    injects a symbol "Xxx.foo$ret.i" into the assembly code stream, where
+    "i" is a running integer (one such symbol is generated for each "call"
+    command within "foo").
+    This symbol is used to mark the return address within the caller's
+    code. In the subsequent assembly process, the assembler translates this
+    symbol into the physical memory address of the command immediately
+    following the "call" command.
+
+      Args:
+        function_name (str): the name of the function to call.
+        n_args (int): the number of arguments of the function.
+    """
+    # This is irrelevant for project 7,
+    # you will implement this in project 8!
+    pass
+
+  def write_return(self) -> None:
+    """
+    Writes assembly code that affects the return command.
+    """
+    # This is irrelevant for project 7,
+    # you will implement this in project 8!
+    pass

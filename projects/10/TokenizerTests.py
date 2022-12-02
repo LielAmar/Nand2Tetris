3d1e8e3dc07c9d5303645542f4f0f70ec9905f50
Liel Amar <liel@lielamar.com>
2022-12-03 01:05:02 +0200

    submitted ex10

--- projects/10/TokenizerTests.py
+++
@@ -1,9 +0,0 @@
-text = "if (x <0) {\n   \n// some comments\n" \
-    + "let    state =   \"negative something\" ;   // yay, it works!\n" \
-    + "if (\"hello\" == \"/*hello*/\") {}" \
-    + "/* multi\nline\ncomment\n */\n}"
-
-from JackTokenizer import JackTokenizer
-
-if __name__ == "__main__":
-    tokenizer = JackTokenizer(text)
